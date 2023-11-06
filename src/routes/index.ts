dbde790d502cd182b6b0e1f67e771c0a72baf180
Joice Martins <joicemartins.dev@gmail.com>
2023-11-06 00:35:02 -0300

    adicionando validação método put

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import ProdutosController from "../controllers/ProductController";
-import { validationPost } from "../middleware/validation";
+import { validationPost, validationPut } from "../middleware/validation";
 
 const route = Router()
 
@@ -8,5 +8,6 @@ route.get('/api/produtos', ProdutosController.showAll)
 route.get('/api/produtos/:id', ProdutosController.findeOne)
 route.delete('/api/produtos/:id', ProdutosController.deleteProduct)
 route.post('/api/produtos', validationPost, ProdutosController.newProduct)
+route.put('/api/produtos/:id', validationPut, ProdutosController.updateProduct)
 
 export default route;
